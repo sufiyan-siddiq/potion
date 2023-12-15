84f645b64ba2dbc9eae8f72d6a5cb35f459b01ff
KSPL <sufiyan.corporate@gmail.com>
2023-12-15 17:35:24 +0530

    orm postgres

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,8 @@
 import prisma from "@/prisma"
-
+import {db} from '@vercel/postgres'
 export const connectToDB = async ()=>{
     try {
+        await db.connect()
         await prisma.$connect();
     } catch (error:any) {
         return new Error(error.message)
